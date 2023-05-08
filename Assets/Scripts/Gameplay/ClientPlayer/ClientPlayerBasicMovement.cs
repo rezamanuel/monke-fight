4feb95c055ba300190bbb18f45182d1a961f6431
Reza Mirzaiee <rezasharp12@gmail.com>
2023-05-07 22:57:14 -0400

    updated Movement + fixed jump bug

--- Assets/Scripts/Gameplay/ClientPlayer/ClientPlayerBasicMovement.cs
+++ Assets/Scripts/Gameplay/ClientPlayer/ClientPlayerBasicMovement.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.InputSystem;
-
+[RequireComponent(typeof(PlayerInput))]
 public class ClientPlayerBasicMovement : NetworkBehaviour 
 
 {
     [SerializeField] private float MoveSpeed = 11f;
-    Vector2 m_MousePosition;
+    [SerializeField] Vector2 m_MousePosition;
+    [SerializeField] Vector3 m_MouseWorldPosition;
     [SerializeField] Vector2 m_MovementInput;
     [SerializeField] Vector3 m_Velocity;
-    [SerializeField] float jumpHeight = 11f;
-    [SerializeField] float lerpPercent;
-    [SerializeField] float jumpDecay;
+    [SerializeField] float jumpVelocity = 11f;
+    [SerializeField] float jumpPower;
+    [SerializeField] float movementLerpPercent;
+    [SerializeField] float gravityLerpPercent;
+    [SerializeField] float jumpDecayTime;
     [SerializeField] bool isTouchingGround=true;
     [SerializeField] bool isJumping;
     [SerializeField] float gravity = 9.81f;
@@ -22,7 +25,7 @@ public class ClientPlayerBasicMovement : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(!IsOwner) enabled = false;
+        jumpPower = jumpVelocity;
     }
 
     void OnMove(InputValue value){
@@ -32,36 +35,60 @@ public class ClientPlayerBasicMovement : NetworkBehaviour
         m_Velocity.x =  MoveSpeed * m_MovementInput.x * Time.fixedDeltaTime; //velocity per frame
     }
     void OnLook(){
-        m_MousePosition += Mouse.current.position.ReadValue();
+        m_MousePosition= Mouse.current.position.ReadValue();
+    }
+    void OnFire(){
+        m_MouseWorldPosition = m_MousePosition;
+        m_MouseWorldPosition.z = Camera.main.nearClipPlane + 1;
+        m_MouseWorldPosition = Camera.main.ScreenToWorldPoint(m_MouseWorldPosition);
+        m_MouseWorldPosition.z = 0;
+    }
+    override public void OnNetworkSpawn(){
+        if (!IsClient || !IsOwner)
+            {
+                GetComponent<PlayerInput>().enabled = false;
+                enabled = false;
+                // dont need to do anything else if not the owner
+                return;
+            }
     }
     void OnCollisionEnter(Collision c){
         isTouchingGround = true;
         isJumping = false;
         m_Velocity.y = 0;
+        jumpPower = jumpVelocity;
         Debug.Log("homola homoula");
     }void OnCollisionExit(Collision c){
         isTouchingGround = false;
     }
-    void OnJump(InputValue value){
-        if(!isTouchingGround && !isJumping) return;
-        Debug.Log("FUCKEN EY");
-        isJumping = value.isPressed;
+    void OnJump(InputValue value)
+    {
+        if (!isTouchingGround && !isJumping) return;
+        else
+        {
+            isJumping = value.isPressed;
+        }
+
     }
     void FixedUpdate()
     {
-        if(!IsOwner) return;
-        if(m_MovementInput.x == 0) 
-            if(Mathf.Abs(m_Velocity.x) < .01f ){
+        if (m_MovementInput.x == 0)
+            if (Mathf.Abs(m_Velocity.x) < .01f)
+            {
                 m_Velocity.x = 0;
             }
             else
             {
-                m_Velocity.x = Mathf.Lerp(m_Velocity.x, 0, lerpPercent * .01f);
+                m_Velocity.x = Mathf.Lerp(m_Velocity.x, 0, movementLerpPercent * .01f);
             }
-        if(isJumping)
-            m_Velocity.y = Mathf.Lerp(jumpHeight * Time.fixedDeltaTime,0, jumpDecay*.01f);
-        else{
-            m_Velocity.y = Mathf.Lerp(m_Velocity.y, -gravity*Time.fixedDeltaTime,jumpDecay*.01f);
+        if (isJumping && jumpPower > 0){
+            
+            m_Velocity.y = Mathf.Lerp(jumpPower, 0, jumpDecayTime * .01f);
+            jumpPower -= jumpDecayTime * Time.fixedDeltaTime;
+            
+        }
+        else if(!isTouchingGround){
+            m_Velocity.y = Mathf.Lerp(m_Velocity.y, -gravity*Time.fixedDeltaTime,Time.fixedDeltaTime*gravityLerpPercent*.01f);
         }
         transform.position += m_Velocity;
     }
